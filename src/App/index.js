3cb22409f06bd494b27659f171b970acbe477a03
Francisco Ruiz <fruiz.rc@gmail.com>
2021-09-23 11:13:46 -0400

    restore the file post example of component composition

--- src/App/index.js
+++ src/App/index.js
@@ -5,46 +5,11 @@ import { TodoProvider } from "../TodoContext";
 // import "./App.css";
 
 function App() {
-  const [state, setState] = React.useState("Estado compartido");
   return (
-    <>
-      <TodoHeader>
-        <TodoCounter />
-        <TodoSearch />
-      </TodoHeader>
-      <TodoList>
-        <TodoItem state={state} />
-      </TodoList>
-    </>
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
   );
 }
-
-function TodoHeader({ children }) {
-  return <header>{children}</header>;
-}
-
-function TodoList({ children }) {
-  return <section className="TodoList-container">{children}</section>;
-}
-
-function TodoCounter() {
-  return <p>TodoCunter</p>;
-}
-
-function TodoSearch() {
-  return <p>TodoSearch</p>;
-}
-
-function TodoItem({ state }) {
-  return <p>{state}</p>;
-}
-
-// function App() {
-//   return (
-//     <TodoProvider>
-//       <AppUI />
-//     </TodoProvider>
-//   );
-// }
 
 export default App;
